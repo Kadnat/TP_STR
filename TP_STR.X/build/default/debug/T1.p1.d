dbdf8ac9ebfcb38260b9568fd5131ab92fc1cdd2
Kadnat <nathanael.blavoballarin@viacesi.fr>
2024-10-14 17:45:05 +0200

    First commit.
    Signed-off-by: Kadnat <nathanael.blavoballarin@viacesi.fr>

---
+++ TP_STR.X/build/default/debug/T1.p1.d
@@ -0,0 +1,15 @@
+build/default/debug/T1.p1:  \
+T1.c  \
+T1.h  \
+main.h  \
+systeme.h  \
+variables_globales.h  \
+afficheur.h  \
+semaphore.h  \
+stid.h  \
+rxtx.h  \
+T2.h  \
+T3.h  \
+T4.h  \
+T5.h  \
+T6.h 
---
+++ TP_STR.X/build/default/debug/T6.p1.d
@@ -0,0 +1,15 @@
+build/default/debug/T6.p1:  \
+T6.c  \
+T6.h  \
+main.h  \
+systeme.h  \
+variables_globales.h  \
+afficheur.h  \
+semaphore.h  \
+stid.h  \
+rxtx.h  \
+T1.h  \
+T2.h  \
+T3.h  \
+T4.h  \
+T5.h 
