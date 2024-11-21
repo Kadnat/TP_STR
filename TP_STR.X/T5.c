005264f57b180c7ec7b189d5dec9adf4941c15f8
Kadnat <nathanael.blavoballarin@viacesi.fr>
2024-11-21 11:58:13 +0100

    Modification tache UART
    Signed-off-by: Kadnat <nathanael.blavoballarin@viacesi.fr>

--- TP_STR.X/T5.c
+++ TP_STR.X/T5.c
@@ -2,43 +2,9 @@
 
 void tache5(void)
 {
-    unsigned int a;
-    unsigned char ma_tache = TACHE5; // Identifiant de la tâche
 
     while(1)
     {
-        // Essaie d'acquérir le sémaphore
-        while (semaphore_tryacquire(ma_tache) == 0)
-        {
-            // Si le sémaphore n'est pas disponible, 
-            // on attend passivement
-            // (l'ordonnanceur passera à une autre tâche)
-        }
 
-        // Une fois le sémaphore acquis, on peut écrire
-        while (PIR1bits.TX1IF==0);   TXREG1='t';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='a';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='c';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='h';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='e';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='5';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='_';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='e';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='n';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='_';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='c';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='o';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='u';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='r';while (TXSTA1bits.TRMT==0);
-        while (PIR1bits.TX1IF==0);   TXREG1='s';while (TXSTA1bits.TRMT==0);
-
-        // Libère le sémaphore après utilisation
-        semaphore_release();
-
-        // Délai
-        for (a=0;a<65000;a++)
-            ;
-        for (a=0;a<65000;a++)
-            ;
     }
 }
--- TP_STR.X/T6.c
+++ TP_STR.X/T6.c
@@ -2,24 +2,49 @@
 
 void tache6(void)
 {
-    unsigned char n;
+   unsigned int a;
+   unsigned char ma_tache = TACHE6; // Identifiant de la tâche
+   unsigned char analog_c = 0, drive_c = 0, forks_c = 0, speed_c = 0, 
+           water_c = 0, oil_c = 0, battery_c = 0, key_c = 0; 
+
     while(1)
     {
-        n=lecture_normale(badge);
-        if (n>0)
+        // Essaie d'acquérir le sémaphore
+        while (semaphore_tryacquire(ma_tache) == 0)
         {
-            if(n<10)
-            {
-                n_octet_badge=n;
-            }
-            else
-            {
-                n_octet_badge=0;
-            }
+            // Si le sémaphore n'est pas disponible, 
+            // on attend passivement
+            // (l'ordonnanceur passera à une autre tâche)
         }
-        else
-        {
-            n_octet_badge=0;
-        }
+        
+
+        // Une fois le sémaphore acquis, on peut écrire
+        if(n_octet_badge == 0) key_c = 0;
+        else key_c = 1;
+        
+        analog_c = 0x0F & ( ((key_c | 0xFE) << 3 ) | ((SIEGE | 0xFE) << 2 ) | 
+                ((FREIN_A_MAIN | 0xFE) << 1 ) | (CHOC | 0xFE) )
+        
+        
+        
+        while (PIR1bits.TX1IF==0);   TXREG1='t';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='a';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='c';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='h';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='e';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='5';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='_';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='e';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='n';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='_';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='c';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='o';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='u';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='r';while (TXSTA1bits.TRMT==0);
+        while (PIR1bits.TX1IF==0);   TXREG1='s';while (TXSTA1bits.TRMT==0);
+
+        // Libère le sémaphore après utilisation
+        semaphore_release();
+
     }
 }
